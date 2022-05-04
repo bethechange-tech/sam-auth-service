5a647f5aca6c611771907bfc0094606f423efc9a
Vito Loso <vitoloso@Vitos-MacBook-Pro.local>
2022-05-04 07:46:58 +0100

    [ improved  logging with winston logging npm package ]

--- src/application/services/logging.ts
+++ src/application/services/logging.ts
@@ -10,33 +10,33 @@ class Logger {
       defaultMeta: { service: 'auth-service' },
       transports: new winston.transports.Console({}),
     })
-    
+
     this.meta = meta
   }
 
   error(message: string, meta?: Record<string, any>) {
     console.log('that should be logging the errors')
-    this.logger.error(message, this._merged(meta))
+    this.logger.error(message, this.merged(meta))
   }
 
   warn(message: string, meta?: Record<string, any>) {
-    this.logger.warn(message, this._merged(meta))
+    this.logger.warn(message, this.merged(meta))
   }
 
   info(message: string, meta?: Record<string, any>) {
-    this.logger.info(message, this._merged(meta))
+    this.logger.info(message, this.merged(meta))
   }
 
   debug(message: string, meta?: Record<string, any>) {
-    this.logger.debug(message, this._merged(meta))
+    this.logger.debug(message, this.merged(meta))
   }
 
-  _merged(extra: Record<string, any> | undefined) {
-    if (extra == undefined) {
+  private merged(extra: Record<string, any> | undefined) {
+    if (!extra) {
       return this.meta
-    } else {
-      return Object.assign(this.meta, extra)
     }
+    
+    return Object.assign(this.meta, extra)
   }
 }
 
